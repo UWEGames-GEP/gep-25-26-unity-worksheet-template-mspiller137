8434349bbecc0aad310b0cccf9c748de7bac6e60
Mason Spiller <mason2.spiller@live.uwe.ac.uk>
2025-11-10 23:19:02 +0000

    Value tweaks for character and scripts

---
+++ Assets/Scripts/PlayerCharacterBehaviour.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class PlayerCharacterBehaviour : MonoBehaviour//, ThirdPersonController
+{
+    //private void OnPause(InputValue value)
+    //{
+    //    if (value.isPressed)
+    //    {
+    //        Debug.Log("Pause Game");
+    //    }
+    //}
+}
